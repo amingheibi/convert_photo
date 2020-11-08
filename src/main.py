3db87525f142408b7c1e2397ae467771b52b2f1c
amingheibi <amin.gheibi@gmail.com>
2020-11-08 02:05:43 -0500

    Add delaunay function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw, ImageStat, ImageFilter
 from pathlib import Path
-from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.spatial import Voronoi, voronoi_plot_2d, Delaunay
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -33,7 +33,7 @@ def extract_point(edges_img, threshold):
     '''threshold needs to be a tuple'''
     width, height = edges_img.size
     result_img = np.zeros((width-2, height-2))
-    voronoi_sites =[]
+    voronoi_sites = np.empty((0,2), int)
     points = list(edges_img.getdata())
 
     # I want to ignore the white box around the edge image
@@ -42,16 +42,23 @@ def extract_point(edges_img, threshold):
         for j, t in enumerate(p):
             if t > threshold:
                 # print('{} , {}'.format(i,j))
-                voronoi_sites.append([i,j])
+                voronoi_sites = np.append(voronoi_sites,[[i,j]] , axis=0)
                 result_img[i][j] = 255
     return result_img, voronoi_sites
 
 
 def draw_voronoi(sites):
     vor = Voronoi(sites)
-    fig = voronoi_plot_2d(vor)
+    voronoi_plot_2d(vor, show_vertices = False, show_points = False)
     plt.show()
 
+
+def draw_delaunay(sites):
+    tess = Delaunay(sites)
+    tri = tess.vertices
+    plt.triplot(sites[:,0], sites[:,1], tri)
+    plt.plot(sites[:,0], sites[:,1], 'o')
+    plt.show()
 
 def main():
     img = Image.open(Path('data/portrait.jpg'))
@@ -59,11 +66,11 @@ def main():
     # img_ht.show()
     img_edges = img.filter(ImageFilter.FIND_EDGES)
     img_edges.show()
-    pixels, sites = extract_point(img_edges,(100,100,100))
+    pixels, sites = extract_point(img_edges,(200,200,200))
     img = Image.fromarray(pixels)
     img.show()
     draw_voronoi(sites)
-    
+    draw_delaunay(sites)
 
 
 if __name__ =="__main__":
